0987cfd284fc558cdc5bf2cd46c4091eae252e6c
MoazzamAliSE <moazzamali6040@gmail.com>
2022-04-16 01:00:01 +0500

    1st day

--- chapter1/first.py
+++ chapter1/first.py
@@ -1,2 +1,5 @@
 print("Great you have started Python ...")
-print ("Try to give one hour daily to  your programming skills")
+print("Try to give one hour daily to  your programming skills")
+
+# search built in module on internet
+#  REPl which stands for "read–evaluate–print loop".  # 26 mins
